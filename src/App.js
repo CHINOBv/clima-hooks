f616b45ca394f0963f23a12d350719b40fe03fe6
chinobv <retrochanel97@gmail.con>
2020-04-18 01:14:33 -0500

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Header from './components/Header.jsx';
 import Form from './components/Form.jsx';
+import Res from './components/Res.jsx';
+import Error from './components/Error.jsx';
 
-import axios from 'axios';
 
 function App() {
 
@@ -11,7 +14,8 @@ function App() {
 	const [ciudad, setCiudad] = useState('');
 	const [pais, setPais] = useState('');
 	
-	const [response, setRespnse] = useState({})
+	const [response, setRespnse] = useState({});
+	const [error, setError] = useState(false);
 
 	const consultDatas = datas => {
 		//console.log(datas);
@@ -20,6 +24,7 @@ function App() {
 		setPais(datas.pais);
 		
 	}
+	
 
 	useEffect(() => {
 		
@@ -27,23 +32,52 @@ function App() {
 
 		let keyAPI = "171636f1e57033ef063ae3271d79b74e";
 		const consultAPI = async() => {
-
+			
 			const API = await `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${keyAPI}`;
+			let res;
+			try {
+				
 
-			const res = await axios.get(API);
-
+			res = await axios.get(API);
 			setRespnse(res.data);
+
+			} catch(e) {
+				// statements
+				setError(true);
+			}
+
+
 		}
+		setError(false)
 
 		consultAPI();
 
+
 	}, [ciudad, pais]);
 	
+	let resComponent;
+
+	if(ciudad === '' || pais === ''){
+		resComponent = null;
+	}else if (error) {
+		resComponent = <Error mensaje="La Ciudad y el Pais no coinciden o no se Encuentran en nuestros Registros" />;
+	}else{
+		resComponent = <Res
+							response={response}
+						/>;
+	}
 
   return (
     <>
-      <Header titulo="titulo"/>
-      <Form consultDatas={consultDatas} />
+        <div className="contenedor-form">
+	    	<div className="container">
+	    		<div className="row">
+			  		<Header titulo="titulo"/>
+			  		<Form consultDatas={consultDatas} />
+			      	{ resComponent }
+				</div>
+			</div>
+		</div>
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -32,49 +32,45 @@ const Form = ({consultDatas}) => {
 	}
 
 	return (
-        <div className="contenedor-form">
-        	<div className="container">
-        		<div className="row">
-        			<div className="col s12 m6">
-						<form
-							onSubmit={consultClima}
-						>
-							<div className="input-field col s12">
-								<label htmlFor="ciudad">Ciudad:</label>
-								<input 
-									type="text"
-									name="ciudad"
-									id="ciudad"
-									onChange={handleChange}
-								/>
-							</div>
-							<div className="input-field col s12">
-								<select 
-									name="pais" 
-									onChange={handleChange}
-								>
-									<option value="">Selecciona un Pais...</option>
-									<option value="US">Estados Unidos</option>
-									<option value="MX">Mexico</option>
-									<option value="AR">Argentina</option>
-									<option value="CO">Colombia</option>
-									<option value="CR">Costarica</option>
-									<option value="ES">España</option>
-									<option value="PE">Peru</option>
-								</select>
-							</div>
-							<div className="input-field col s12">
-								<input 
-									disabled={validForm()}
-									type="submit"
-									className="waves-effect waves-light btn-large btn-block yellow accent-4" 
-									value="Buscar Clima"/>
-							</div>
-						</form>
-					</div>
+
+		<div className="col s12 m6">
+			<form
+				onSubmit={consultClima}
+			>
+				<div className="input-field col s12">
+					<label htmlFor="ciudad">Ciudad:</label>
+					<input 
+						type="text"
+						name="ciudad"
+						id="ciudad"
+						onChange={handleChange}
+					/>
 				</div>
-			</div>
+				<div className="input-field col s12">
+					<select 
+						name="pais" 
+						onChange={handleChange}
+					>
+						<option value="">Selecciona un Pais...</option>
+						<option value="US">Estados Unidos</option>
+						<option value="MX">Mexico</option>
+						<option value="AR">Argentina</option>
+						<option value="CO">Colombia</option>
+						<option value="CR">Costarica</option>
+						<option value="ES">España</option>
+						<option value="PE">Peru</option>
+					</select>
+				</div>
+				<div className="input-field col s12">
+					<input 
+						disabled={validForm()}
+						type="submit"
+						className="waves-effect waves-light btn-large btn-block yellow accent-4" 
+						value="Buscar Clima"/>
+				</div>
+			</form>
 		</div>
+
 	)
 }
 
